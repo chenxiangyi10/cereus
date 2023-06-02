263d62bfb989af4b57d5661279a03faee8e3357b
chenxiangyi10 <chenxiangyi10@gmail.com>
2023-06-02 13:28:00 +0800

    add prompts template module

---
+++ prompts.py
@@ -0,0 +1,33 @@
+from typing import Optional
+class Prompt:
+    def __init__(self, model_name: str) -> None:
+        self.model_name = model_name
+        self.prompt_elements = {
+            "User_name": '',
+            "AI_name": '',
+            "separator": '',
+        }
+        self.isBare = False
+    def set_prompt_elements(self, user: str = '', ai: str = '', separator: str = '') -> None:
+        self.prompt_elements["User_name"] = user
+        self.prompt_elements["AI_name"] = ai
+        self.prompt_elements["separator"] = separator
+    def get_prompt(self, user_message: str, response_prefix: Optional[str] = None) -> str:
+        if self.isBare:
+            return user_message
+        if response_prefix is None:
+            prompt = f"{self.prompt_elements['User_name']}: {user_message}{self.prompt_elements['separator']}{self.prompt_elements['AI_name']}:"
+        else:
+            prompt = f"{self.prompt_elements['User_name']}: {user_message}{self.prompt_elements['separator']}{self.prompt_elements['AI_name']}: {response_prefix}"
+        return prompt
+
+    
+class PromptGuanaco(Prompt):
+    def __init__(self) -> None:
+        super().__init__('Guanaco')
+        self.set_prompt_elements('### Human', '### Assistant', '\n')
+    
+class PromptWizard_vicuna(Prompt):
+    def __init__(self) -> None:
+        super().__init__('wizard_vicuna')
+        self.set_prompt_elements('User', 'Assistant', '\n\n')
